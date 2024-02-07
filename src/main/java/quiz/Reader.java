9a6c964e69130d8882d425e4093589cedd63a1e6
Adam Cervenka <cervenkaadamek@seznam.cz>
2024-02-07 13:48:26 +0100

    updated Reader

--- src/main/java/quiz/Reader.java
+++ src/main/java/quiz/Reader.java
@@ -1,21 +1,29 @@
 package quiz;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.ArrayList;
+
 public class Reader {
-    public ArrayList<Question> questions;
-    public ArrayList<Question> ReadQuestions(String path) {
+    private ArrayList<Question> questions;
+    public ArrayList<Question> getQuestions() {
+        return questions;
+    }
+    public void ReadQuestions(String path) {
+
         try (BufferedReader reader = new BufferedReader(new java.io.FileReader(path))) {
+
             Question question;
             questions = new ArrayList<Question>();
+            
             while ((question = readQuestion(reader)) != null){
                 questions.add(question);
             }
-        } catch (Exception e) {
+
+        } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
-        return questions;
     }
-    public void printQuestions() {
+    public void printAllQuestions() {
         for (Question question : questions) {
             System.out.println(question);
         }
@@ -23,16 +31,30 @@ public class Reader {
     private Question readQuestion(BufferedReader reader) {
         try {
             String category = reader.readLine();
+
+            // If the file ends, return null
+            if (category == null) {
+                return null;
+            }
+            // Skip empty lines
+            while (category.matches("\\s*")) {
+                category = reader.readLine();
+                if (category == null) {
+                    return null;
+                }
+            }
+
             String question = reader.readLine();
             String correctAnswer = reader.readLine();
             ArrayList<String> options = new ArrayList<String>();
             String line;
+
             while ((line = reader.readLine()) != null && !line.equals("")) {
                 options.add(line);
             }
             return new Question(category, question, options.toArray(new String[options.size()]), correctAnswer);
             
-        } catch (Exception e) {
+        } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
             return null;
         }
