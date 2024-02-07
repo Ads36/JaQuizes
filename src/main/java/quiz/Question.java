0cc640ea84b6829d6e4e4e7e596932d7233ec247
Adam Cervenka <cervenkaadamek@seznam.cz>
2024-02-07 13:48:11 +0100

    added Quiz

--- src/main/java/quiz/Question.java
+++ src/main/java/quiz/Question.java
@@ -5,13 +5,21 @@ public class Question {
     String question;
     String[] options;
     int correctAnswer;
+    
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         //sb.append(category + "\n");
         sb.append(question + "\n");
         for (int i = 0; i < options.length; i++) {
-            sb.append((char)('A' + i) + ") " + options[i]);
+
+            if (i < 26) {
+                sb.append((char)('A' + i) + ") ");
+            }
+            else {
+                sb.append((char)('A' + (i / 26) - 1) + "" + (char)('A' + (i % 26)) + ") ");
+            }
+            sb.append(options[i]);
             if (i != options.length - 1) {
                 sb.append("\n");
             }
@@ -23,21 +31,51 @@ public class Question {
         this.category = category;
         this.question = question;
         this.options = options;
-        this.correctAnswer = getCorrectAnswerAsInt(correctAnswer)-1;
+        this.correctAnswer = getCorrectAnswerAsInt(correctAnswer);
     }
-    private int getCorrectAnswerAsInt(String answer) {
+    private int getCorrectAnswerAsInt(String answer) throws IllegalArgumentException {
         int answerAsInt = 0;
         if (answer.matches("\\d+")) {
-            answerAsInt =  Integer.parseInt(answer);
-        } else if (answer.length() == 1) {
+            answerAsInt =  Integer.parseInt(answer) - 1;
+        } 
+        else if (answer.length() == 1) {
             if (answer.charAt(0) >= 'A' && answer.charAt(0) <= 'Z') {
                 answer = answer.toLowerCase();
             }
             answerAsInt = answer.charAt(0) - 'a';
         } 
-        if (answerAsInt < 1 || answerAsInt > this.options.length) {
+        else if (answer.length() == 2) {
+            if (answer.charAt(0) >= 'A' && answer.charAt(0) <= 'Z') {
+                answer = answer.toLowerCase();
+            }
+            char firstChar = answer.charAt(0);
+            char secondChar = answer.charAt(1);
+            
+            if (firstChar < 'a' || firstChar > 'z' || secondChar < 'a' || secondChar > 'z') {
+                throw new IllegalArgumentException("Invalid answer: " + answer);
+            }
+            
+            answerAsInt = (firstChar - 'a' + 1) * 26 + (secondChar - 'a');
+        }
+        else {
+            throw new IllegalArgumentException("Invalid answer: " + answer);
+        }
+
+        // Check if the answer is in the range of the options
+        if (answerAsInt < 0 || answerAsInt > this.options.length-1) {
             throw new IllegalArgumentException("Invalid answer: " + answer);
         }
         return answerAsInt;
+    }
+    public boolean isCorrectAnswer(String answer) { 
+        return getCorrectAnswerAsInt(answer)== correctAnswer;
+    }
+    public boolean isValidAnswer(String answer) {
+        try {
+            getCorrectAnswerAsInt(answer);
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
     }
 }
--- src/main/java/quiz/Quiz.java
+++ src/main/java/quiz/Quiz.java
@@ -1,5 +1,56 @@
 package quiz;
 
+import java.util.ArrayList;
+
 public class Quiz {
-    
+    ArrayList<Question> allQuestions;
+    public Quiz(ArrayList<Question> questions) {
+        this.allQuestions = questions;
+    }
+    public ArrayList<String> getAllCategories() {
+        ArrayList<String> categories = new ArrayList<String>();
+        for (Question question : allQuestions) {
+            if (!categories.contains(question.category)) {
+                categories.add(question.category);
+            }
+        }
+        return categories;
+    }
+    public ArrayList<Question> getQuestionsByCategories(ArrayList<String> categories) {
+        ArrayList<Question> questionsByCategories = new ArrayList<Question>();
+        for (Question question : allQuestions) {
+            if (categories.contains(question.category)) {
+                questionsByCategories.add(question);
+            }
+        }
+        return questionsByCategories;
+    }
+    public ArrayList<Question> shuffleQuestions(ArrayList<Question> questions) {
+        ArrayList<Question> shuffledQuestions = new ArrayList<Question>();
+        while (questions.size() > 0) {
+            int randomIndex = (int)(Math.random() * questions.size());
+            shuffledQuestions.add(questions.get(randomIndex));
+            questions.remove(randomIndex);
+        }
+        return shuffledQuestions;
+    }
+    public void startQuiz(ArrayList<Question> questions) {
+        int score = 0;
+        for (Question question : questions) {
+            System.out.println(question);
+            System.out.print("Answer: ");
+            String answer = System.console().readLine();
+            while (!question.isValidAnswer(answer)) {
+                System.out.println("Invalid answer: " + answer);
+                System.out.print("Zkuste to znovu: ");
+                answer = System.console().readLine();
+            }
+            if (question.isCorrectAnswer(answer)) {
+                score++;
+            }
+            
+        }
+        System.out.println("Your score: " + score + "/" + questions.size() + ", " + (int)((double)score / questions.size() * 100) + "%");
+    }
+
 }
